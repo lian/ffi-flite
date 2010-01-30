7f152146843dfe723fcb215780a4bb2f01a8d8e6
Julian Langschaedel <meta.rb@gmail.com>
2010-01-30 01:26:57 +0100

    Add EM::Queue

--- flite.rb
+++ flite.rb
@@ -4,9 +4,13 @@ require 'fileutils'
 module FliteSpeak
   # none  /tmp  tmpfs  nodev,nosuid,noexec,nodiratime,size=256M  0 0
   TMP_PATH = '/tmp/flite_fifo'
+  F_FILE_BIN = "/usr/bin/flite -o play -f %s -ps "
+  F_STR_BIN  = "/usr/bin/flite -o play -t \"%s\" -ps "
+  MsgQueue = EM::Queue.new
+
 
   def file_modified
-    SpeakDev.read_file(TMP_PATH) unless File.size(TMP_PATH) == 0
+    push_file TMP_PATH
   end
 
   def file_deleted
@@ -17,106 +21,59 @@ module FliteSpeak
     puts 'fifo unbind'; EM.stop
   end
 
-  module SpeakDev
-    F_FILE_BIN = "/usr/bin/flite -o play -f %s -ps "
-    F_STR_BIN  = "/usr/bin/flite -o play -t \"%s\" -ps"
-    LOG_TEMPLATE = "\n--> Process Event: %s  %i\nflite input: %s\n"
 
+  def push_file(filepath, flush=true)
+    return nil if File.size(filepath) == 0
+
+    MsgQueue.push File.read(filepath)
+    File.open(filepath, 'w'){ |f| f.print '' } if flush
+
+    process_queue( filepath + '.pb' )
+  end
+
+  def process_queue(path, c=1)
+    #p '--process_queue %s, %i' % [path, c]
+
+    if SpeakDev::State[:lock] == true
+      EM::Timer.new(0.5){process_queue(path, c+1)}
+    else
+      MsgQueue.pop do |v|  log_input(v)
+        # write flite input file
+        File.open(path, 'w'){|f| f.print v }
+        # exec flite
+        EM.popen(F_FILE_BIN % [path], SpeakDev)
+        #EM::Timer.new(0.5){process_queue(path, c+1)} unless MsgQueue.empty?
+      end
+    end
+  end
+
+
+  module SpeakDev
+    State =  { :lock => false }
     def receive_data data
       puts "flite spoke: #{data}"
     end
-
+    def post_init
+      State[:lock] = true
+    end
     def unbind
-      #puts "flite exit: #{get_status.exitstatus}"
-    end
-
-    def self.flush_input_file(path)
-      File.open(path, 'w'){ |f| f.print '' }
-    end
-
-    def self.log_input(path)
-      print LOG_TEMPLATE % [path, Time.now.to_i, File.read(path).inspect]
-    end
-
-    def self.read_file(filepath, flush=true)
-      if flush
-        path = filepath + '.pb'
-        FileUtils.cp filepath, path
-        flush_input_file(filepath)
-      else
-        path = filepath
-      end
-
-      log_input(path)
-      #p(F_FILE_BIN % [path])
-      EM.popen(F_FILE_BIN % [path], self)
-    end
-
-    def self.from_string(str)
-      EM.popen(F_STR_BIN % [path], self)
+      State[:lock] = false
+      State[:exitcode] = get_status.exitstatus
     end
   end
+
+  LOG_TEMPLATE = "\n--> Process Event: %s  %i\nflite input: %s\n"
+  def log_input(msg)
+    print LOG_TEMPLATE % ['mem', Time.now.to_i, msg.inspect]
+  end
+
+  #def self.from_string(str)
+  #  EM.popen(F_STR_BIN % [path], self)
+  #end
 end
+
 
 EM.run do
-  #EM.start_unix_domain_server '/tmp/flite-speak', FliteQueueListen
-
-  flite_watch = EM.watch_file(FliteSpeak::TMP_PATH, FliteSpeak)
+  flite_fifo = EM.watch_file(FliteSpeak::TMP_PATH, FliteSpeak)
 end
 
-
-
-__END__
-module FliteQueueListen
-  MsgQueue = EM::Queue.new
-  def post_init
-    puts "-- someone connected to the echo server!"
-  end
-
-  def receive_data data
-    send_data ">>>you sent: #{data}"
-    MsgQueue.push data
-    close_connection if data =~ /quit/i
-  end
-
-  def unbind
-    puts "-- someone disconnected from the echo server!"
-  end
-end
-
-
-module FifoFile
-  TMP_PATH = '/tmp/flite_fifo'
-  MsgQueue = EM::Queue.new
-
-  File.open(TMP_PATH, 'w'){ |f| f.print '' }
-
-  def self.fifo_read
-    file = File.read(TMP_PATH)
-    #p file
-    unless file.empty?
-      puts 'read and flushing fifo..'
-      File.open(TMP_PATH, 'w'){ |f| f.print '' }
-      file.split("\n").each{|msg| MsgQueue.push msg }
-    end
-  end
-  
-  def file_modified
-    if res = FifoFile.fifo_read
-      res.each do |msg|
-        p msg;  EM.stop if msg == ':,quit'
-
-        # speak it
-        FliteSpeaker.say(msg)
-      end
-    end
-  end
-
-  def file_deleted
-    puts 'fifo deleted'
-  end
-  def unbind
-    puts 'fifo unbind'
-    EM.stop
-  end
-end
